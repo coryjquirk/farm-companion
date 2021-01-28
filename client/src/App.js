c472605683ec977e3658fed64406bb06275c7d0b
Cory Quirk <coryjquirk@gmail.com>
2021-01-28 13:01:17 -0600

    register page fix

--- client/src/App.js
+++ client/src/App.js
@@ -28,25 +28,27 @@ import FrogtownHistory from "./components/FrogtownHistory";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import ScrollArrow from "./components/ScrollArrow";
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
 
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
+// Check for token to keep user logged in
+// if (localStorage.jwtToken) {
+//   // Set auth token header auth
+//   const token = localStorage.jwtToken;
+//   setAuthToken(token);
+//   // Decode token and get user info and exp
+//   const decoded = jwt_decode(token);
+//   // Set user and isAuthenticated
+//   store.dispatch(setCurrentUser(decoded));
+//   // Check for expired token
+//   const currentTime = Date.now() / 1000; // to get in milliseconds
+//   if (decoded.exp < currentTime) {
+//     // Logout user
+//     store.dispatch(logoutUser());
+
+//     // Redirect to login
+//     window.location.href = "./login";
+//   }
+// }
+
 class App extends Component {
   render() {
     return (
